7059a3c2379339ed8170bb7e322a176c4498b088
“sitajaf” <sitajaf@gmail.com>
2016-05-29 15:56:13 +1000

    improve clock

--- src/printer.js
+++ src/printer.js
@@ -1,9 +1,9 @@
 exports.print = function(clockArray){
-    console.log("      " + clockArray[0]);
-    console.log("   " + clockArray[11] + "     " + clockArray[1]);
-    console.log("  " + clockArray[10] + "       " + clockArray[2]);
-    console.log(" " + clockArray[9] + "         " + clockArray[3]);
-    console.log("  " + clockArray[8] + "       " + clockArray[4]);
-    console.log("   " + clockArray[7] + "     " + clockArray[5]);
-    console.log("      " + clockArray[6]);
+    console.log('                   ' + clockArray[0]);
+    console.log('           ' + clockArray[11] + '                ' + clockArray[1] +'\n\n\n');
+    console.log('   ' + clockArray[10] + '                               ' + clockArray[2] +'\n\n\n');
+    console.log(clockArray[9] + '                                      ' + clockArray[3]+ '\n\n\n');
+    console.log('   ' + clockArray[8] + '                               ' + clockArray[4]+'\n\n\n');
+    console.log('           ' + clockArray[7] + '               ' + clockArray[5]);
+    console.log('                   ' + clockArray[6]);
 };
